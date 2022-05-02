8332e242245bc1290998a088e484c2bfd4bf44cf
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 20:02:18 -0300

    Teste do uso do operador de atribuição de maneira composta com o operador de módulo em Java.

---
+++ src/TesteOperadorCompostoModulo.java
@@ -0,0 +1,9 @@
+public class TesteOperadorCompostoModulo {
+
+    public static void main(String[] args) {
+        byte quantidade = 33;
+        quantidade %= 30;
+
+        System.out.printf("Sobra: %d", quantidade);
+    }
+}
